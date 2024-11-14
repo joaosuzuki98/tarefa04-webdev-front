faea1e61f26b1c03fccb5a50220eeeca93e66414
João Suzuki <joaosuzuki98@outlook.com>
2024-11-14 08:41:18 -0300

    [feat] Add table component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import styles from './Input.module.css'
+
+export default class Input extends Component {
+    render() {
+        return(
+            <div>
+                <input type="text" className={`${styles.input} p-2 rounded-2xl`} />
+            </div>
+        )
+    }
+}
