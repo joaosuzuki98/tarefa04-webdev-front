918017676569659047031ea83dd34242ef024692
João Suzuki <joaosuzuki98@outlook.com>
2024-11-13 23:10:16 -0300

    [feat] Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 function App() {
-  return (
-    <div>
-
-    </div>
-  );
+	const handleClick = () => console.log('teste')
+	return (
+		<div className='main-container'>
+			<Button onClickHandle={handleClick}>
+				<i className="fa-solid fa-plus"></i>
+			</Button>
+		</div>
+	);
 }
 
 export default App;
