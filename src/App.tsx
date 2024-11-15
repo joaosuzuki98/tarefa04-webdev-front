541c3cbfb982c18d3176467696f8fcc1996fc2da
João Suzuki <joaosuzuki98@outlook.com>
2024-11-14 23:00:28 -0300

    [feat] Add main layout to mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,37 @@ function App() {
 	const handleClick = () => console.log('teste')
 	return (
 		<div className='main-container'>
-			<NotificationsIcon/>
-			<Icon/>
-			<Button onClickHandle={handleClick}>
-				<i className="fa-solid fa-plus"></i>
-			</Button>
-			<Input/>
-			<Table/>
+			<header className='w-full flex justify-between mb-10'>
+				<Icon/>
+				<NotificationsIcon/>
+			</header>
+
+			<main className='flex flex-col justify-between h-[80vh]'>
+				<div>
+					<div className='w-full flex justify-center'>
+						<Button onClickHandle={handleClick}>
+							<i className="fa-solid fa-plus"></i>
+						</Button>
+					</div>
+
+					<div className='my-5'>
+						<Input/>
+					</div>
+
+					<div className='flex justify-evenly'>
+						<Button onClickHandle={handleClick}>
+							<i className="fa-solid fa-magnifying-glass"></i>
+						</Button>
+						<Button onClickHandle={handleClick}>
+							<i className="fa-solid fa-pencil"></i>
+						</Button>
+						<Button onClickHandle={handleClick}>
+							<i className="fa-solid fa-trash-can"></i>
+						</Button>
+					</div>
+				</div>
+				<Table/>
+			</main>
 		</div>
 	);
 }
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -5,7 +5,7 @@ export default class Icon extends Component {
         return(
             <div>
                 <figure className={`neumorphic-look rounded-full w-[3.6rem] h-[3.6rem] p-4 flex justify-center items-center`}>
-                    <i className="fa-brands fa-github"></i>
+                    <i className="fa-brands fa-github text-4xl"></i>
                 </figure>
             </div>
         )
