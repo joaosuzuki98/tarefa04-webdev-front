3414a5a7ded4378769e5fe79ee6bbc0c63bef288
João Suzuki <joaosuzuki98@outlook.com>
2024-11-17 21:21:45 -0300

    [feat] Add radio buttons to select entity

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Button from './components/Button/Button'
 import ProductsTable from './components/Table/ProductsTable'
@@ -12,6 +12,17 @@ import Modal from './components/Modal/Modal'
 function App() {
 	const handleClick = () => console.log('test')
 	const handleClose = () => console.log('test')
+
+	const [selectedEntity, setSelectedEntity] = useState<'products' | 'suppliers' | 'purchases'>('products')
+	const [isRegisterModalOpen, setIsRegisterModalOpen] = useState<Boolean>(false)
+
+	const openModal = () => {
+		setIsRegisterModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsRegisterModalOpen(false)
+	}
 	return (
 		<div className='main-container'>
 			<header className='w-full flex justify-between mb-10'>
@@ -20,8 +31,48 @@ function App() {
 			</header>
 
 			<main className='flex flex-col justify-between h-[80vh]'>
+				<div className='my-4 flex justify-center gap-4'>
+					<label className='radio-button neumorphic-look flex flex-col items-center'>
+						<input
+							type="radio"
+							name="entity"
+							value="products"
+							checked={selectedEntity === 'products'}
+							onChange={(e) => setSelectedEntity(e.target.value as 'products' | 'suppliers' | 'purchases')}
+							className="hidden"
+						/>
+						<i className="fa-solid fa-box text-lg mb-2"></i>
+						<div className={`circle ${selectedEntity === 'products' ? 'active' : ''}`}></div>
+					</label>
+
+					<label className='radio-button neumorphic-look flex flex-col items-center'>
+						<input
+							type="radio"
+							name="entity"
+							value="suppliers"
+							checked={selectedEntity === 'suppliers'}
+							onChange={(e) => setSelectedEntity(e.target.value as 'products' | 'suppliers' | 'purchases')}
+							className="hidden"
+						/>
+						<i className="fa-solid fa-truck text-lg mb-2"></i>
+						<div className={`circle ${selectedEntity === 'suppliers' ? 'active' : ''}`}></div>
+					</label>
+
+					<label className='radio-button neumorphic-look flex flex-col items-center'>
+						<input
+							type="radio"
+							name="entity"
+							value="purchases"
+							checked={selectedEntity === 'purchases'}
+							onChange={(e) => setSelectedEntity(e.target.value as 'products' | 'suppliers' | 'purchases')}
+							className="hidden"
+						/>
+						<i className="fa-solid fa-receipt text-lg mb-2"></i>
+						<div className={`circle ${selectedEntity === 'purchases' ? 'active' : ''}`}></div>
+					</label>
+				</div>
 				<div>
-					<div className='w-full flex justify-center'>
+					<div className='w-full flex justify-center' onClick={openModal}>
 						<Button onClickHandle={handleClick}>
 							<i className="fa-solid fa-plus"></i>
 						</Button>
@@ -43,12 +94,18 @@ function App() {
 						</Button>
 					</div>
 				</div>
-				<ProductsTable/>
-				<SupplierTable/>
-				<PurchaseHistoryTable/>
-				<Modal onClose={handleClose} onSubmit={handleClick}>
-					<p>test</p>
-				</Modal>
+				{selectedEntity === 'products' && <ProductsTable />}
+				{selectedEntity === 'suppliers' && <SupplierTable />}
+				{selectedEntity === 'purchases' && <PurchaseHistoryTable />}
+
+				{isRegisterModalOpen && (
+					<Modal onClose={closeModal} onSubmit={handleClick}>
+						<Input/>
+						<Input/>
+						<Input/>
+						<Input/>
+					</Modal>
+				)}
 			</main>
 		</div>
 	)
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -16,21 +16,22 @@ export default class Modal extends Component<modalProps, {}> {
             <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
                 <div className="absolute w-full h-full backdrop-blur-md" onClick={onClose}></div>
                 <div className="relative bg-white p-6 rounded-lg shadow-lg z-10 w-[90%]">
-                    {children}
-                    <div className='mt-4 w-full justify-evenly flex'>
-                        <button
-                            className="px-4 py-2 neumorphic-look rounded"
-                            onClick={onClose}
-                        >
-                            Close
-                        </button>                    
-                        <button
-                            className="px-4 py-2 neumorphic-look rounded"
-                            onClick={onSubmit}
-                        >
-                            Submit
-                        </button>
-                    </div>
+                    <form onSubmit={onSubmit}>
+                        {children}
+                        <div className='mt-4 w-full justify-evenly flex'>
+                            <button
+                                className="px-4 py-2 neumorphic-look rounded"
+                                onClick={onClose}
+                            >
+                                Close
+                            </button>                    
+                            <button
+                                className="px-4 py-2 neumorphic-look rounded"
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         )
