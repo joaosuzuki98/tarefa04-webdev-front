e3c307df332d35499217a49c2c3935d2e57aa93e
João Suzuki <joaosuzuki98@outlook.com>
2024-11-18 16:48:40 -0300

    [style] Add opening animation to modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -16,7 +16,7 @@ export default class Modal extends Component<modalProps, {}> {
         return (
             <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
                 <div className="absolute w-full h-full backdrop-blur-md" onClick={onClose}></div>
-                <div className="relative bg-white p-6 rounded-lg shadow-lg z-10 w-[90%]">
+                <div className="relative bg-white p-6 rounded-lg shadow-lg z-10 w-[90%] modal-container">
                     <h2 className='font-bold text-xl'>{title}</h2>
                     <form onSubmit={onSubmit}>
                         {children}
