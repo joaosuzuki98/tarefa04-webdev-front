884864c2e1a5fc5f9241fb108bc1ec5df1df94fa
João Suzuki <joaosuzuki98@outlook.com>
2024-11-20 16:42:03 -0300

    [feat] Add github link

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -4,9 +4,11 @@ export default class Icon extends Component {
     render() {
         return(
             <div>
-                <figure className={`neumorphic-look rounded-full w-[3.6rem] h-[3.6rem] p-4 flex justify-center items-center`}>
-                    <i className="fa-brands fa-github text-4xl"></i>
-                </figure>
+                <a href="https://github.com/joaosuzuki98" target='_blank' rel="noreferrer">
+                    <figure className={`neumorphic-look rounded-full w-[3.6rem] h-[3.6rem] p-4 flex justify-center items-center`}>
+                        <i className="fa-brands fa-github text-4xl"></i>
+                    </figure>
+                </a>
             </div>
         )
     }
